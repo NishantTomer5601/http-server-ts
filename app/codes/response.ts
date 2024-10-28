4942c65a5411317c2c69743497aeee7843c7ddf1
Nishant Tomer <nishanttomer5601@gmail.com>
2024-10-28 16:00:15 +0530

    Restructure

---
+++ app/codes/response.ts
@@ -0,0 +1,11 @@
+import * as net from "net";
+
+
+const server = net.createServer((socket) => {
+    const response = `HTTP/1.1 200 OK\r\n\r\n`;
+    socket.write(response);
+    socket.end();
+    
+});
+
+server.listen(4221, "localhost");
