30ea66e5670a9c9e335ff9cf81119a8e7bbfe46b
Nishant Tomer <nishanttomer5601@gmail.com>
2024-10-28 14:35:41 +0530

    Writing response

--- app/main.ts
+++ app/main.ts
@@ -2,9 +2,9 @@ import * as net from "net";
 
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    const response = `HTTP/1.1 200 OK\r\n\r\n`;
+    socket.write(response);
     socket.end();
-  });
 });
 
 server.listen(4221, "localhost");
